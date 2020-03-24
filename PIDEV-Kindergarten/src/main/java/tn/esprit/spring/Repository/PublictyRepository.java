7f97092de5871561343c67ade0e812b06c886321
manel <manel@DESKTOP-3JUG89P>
2020-03-24 23:00:51 +0100

    test

---
+++ PIDEV-Kindergarten/src/main/java/tn/esprit/spring/Repository/PublictyRepository.java
@@ -0,0 +1,9 @@
+package tn.esprit.spring.Repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import tn.esprit.spring.entity.Publicity;
+
+public interface PublictyRepository extends JpaRepository<Publicity, Integer> {
+
+}
