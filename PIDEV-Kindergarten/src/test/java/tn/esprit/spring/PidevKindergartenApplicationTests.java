5fe8faa17052939b69d01dd5b5777320d078d414
Oussama <Oussama@DESKTOP-UO8LOF4>
2020-03-30 23:00:36 +0200

    corretcion 1

--- PIDEV-Kindergarten/src/test/java/tn/esprit/spring/PidevKindergartenApplicationTests.java
+++ PIDEV-Kindergarten/src/test/java/tn/esprit/spring/PidevKindergartenApplicationTests.java
@@ -5,13 +5,7 @@ import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
-<<<<<<< HEAD
 
-import tn.esprit.spring.Repository.UserRepository;
-import tn.esprit.spring.entity.User;
-@RunWith(SpringRunner.class)
-=======
->>>>>>> branch 'master' of https://github.com/gaaliche22/KINDERGARTEN-PI-DEV.git
 
 import tn.esprit.spring.Services.PublicityServiceImpl;
 
@@ -19,25 +13,19 @@ import tn.esprit.spring.Services.PublicityServiceImpl;
 @RunWith(SpringRunner.class)
 @SpringBootTest
 class PidevKindergartenApplicationTests {
-<<<<<<< HEAD
-	UserRepository userRepository;
-=======
-	
+
 	@Autowired 
 	PublicityServiceImpl ps; 
 	
->>>>>>> branch 'master' of https://github.com/gaaliche22/KINDERGARTEN-PI-DEV.git
+
 	@Test
 	public void contextLoads() {
-<<<<<<< HEAD
-		User user=new User("oussama1","oussamas","gaaliche","123","oussama.gaaliche@gmail.com",(long) 92374131,"0","ADMIN");
+
 		
 		
-		userRepository.save(user);
-=======
-		//Publicity p = new Publicity(); 
-		ps.deletePublicity(1);
->>>>>>> branch 'master' of https://github.com/gaaliche22/KINDERGARTEN-PI-DEV.git
+		
+		
+
 	}
 
 }
