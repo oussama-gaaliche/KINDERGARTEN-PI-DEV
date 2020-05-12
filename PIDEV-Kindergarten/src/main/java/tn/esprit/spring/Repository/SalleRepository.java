259c39218782cddda7c873e13ea073d8b23684c7
manel ben salah <manelbensalah48@gmail.com>
2020-05-12 18:12:58 +0100

    Event Service

---
+++ PIDEV-Kindergarten/src/main/java/tn/esprit/spring/Repository/SalleRepository.java
@@ -0,0 +1,13 @@
+package tn.esprit.spring.Repository;
+
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
+
+import tn.esprit.spring.entity.Salle;
+
+@Repository
+public interface SalleRepository  extends CrudRepository<Salle, Integer>{
+	
+	
+
+}
